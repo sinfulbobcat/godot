319e22ecb4834c54520b0caa08c850651f1fa417
Bobcat <bobcatsinful@gmail.com>
2025-10-25 20:17:07 +0530

    learning godot source

--- editor/editor_log.cpp
+++ editor/editor_log.cpp
@@ -595,7 +595,7 @@ EditorLog::EditorLog() {
 	vb_right->add_child(editor_filter->toggle_button);
 	type_filter_map.insert(MSG_TYPE_EDITOR, editor_filter);
 
-	add_message(GODOT_VERSION_FULL_NAME " (c) 2007-present Juan Linietsky, Ariel Manzur & Godot Contributors.");
+	add_message(GODOT_VERSION_FULL_NAME " (c) 2007-present SINFUL BOBCAT MOTHERFUCKERSSSSSS");
 
 	eh.errfunc = _error_handler;
 	eh.userdata = this;
